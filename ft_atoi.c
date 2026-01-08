172fa93ae452eec1309e78d072f49d34edcb1bf3
G <g@Gs-MacBook-Air.local>
2026-01-08 12:34:13 -0500

    ft_atoi (˵ ͡° ͜ʖ ͡°˵)

--- ft_atoi.c
+++ ft_atoi.c
@@ -12,38 +12,38 @@ RETURN VALUE
 */
 
 #include <stdio.h>
-#include "libft.h"
-
-/* a ** b */
-static int	ft_pow(int a, int b)
-{
-	int	res;
-	
-	res = 1;
-	while (b > 0)
-	{
-		res = res * a;
-		b--;
-	}
-	return (res);
-}
 
 int	ft_atoi(const char *str)
 {
 	int i;
+	int	num;
+	int	sign;
 
+	sign = 1;
+	num = 0;
 	i = 0;
-	while (str[i] < '0' || str[i] > '9')
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
 	{
+		if (str[i] == '-')
+			sign = -1;
 		i++;
 	}
-
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	return (num * sign);
 }
 
+/*
 int main(void)
 {
-	char	csum[] = "-43 gattini";
+	char	csum[] = " -43gattini";
 
 	printf("res: %d\n", ft_atoi(csum));
 	return (0);
 }
+*/
--- run.sh
+++
@@ -1,4 +0,0 @@
-clear
-cc ft_atoi.c -Wall -Wextra -Werror
-./a.out
-rm a.out
