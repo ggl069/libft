bc1f1e80c2d7a229aed13192ed7e7b124ecefc7b
g <gaetanigregorio@gmail.com>
2026-01-15 15:02:10 +0100

    ft_strmap

---
+++ ft_striteri.c
@@ -0,0 +1,58 @@
+// 42 header
+
+#include <stdlib.h>
+#include "libft.h"
+
+/*
+Description:	Applies the function f to each character of the string passed
+				as argument, and passing its index as first argument. Each
+				character is passed by address to f to be modified if necessary.
+
+Param. #1:		The string to iterate.
+Param. #2:		The function to apply to each character of s and its index.
+
+Return value:	None.
+*/
+
+void ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	unsigned int	i;
+
+	if (!s || !f)
+		return;
+	i = 0;
+	while (s[i] != '\0')
+	{
+			f(i, (char *)&s[i]);
+			i++;
+	}
+}
+
+/*
+void    test(unsigned int i, char *c)
+{
+    if (i % 2 == 0)
+    {
+        if (*c >= 'a' && *c <= 'z')
+            *c = *c - 32;
+    }
+    else
+    {
+        if (*c >= 'A' && *c <= 'Z')
+            *c = *c + 32;
+    }
+}
+
+int main(void)
+{
+    char str[] = "Hello World";
+
+    printf("Prima: %s\n", str);
+
+    ft_striteri(str, test);
+
+    printf("Dopo : %s\n", str);
+
+    return (0);
+}
+*/
---
+++ ft_strmap.c
@@ -0,0 +1,58 @@
+// 42 header
+
+#include <stdlib.h>
+#include "libft.h"
+
+/*
+Description:	Applies the function f to each character of the string given
+				as argument to create a “fresh” new string (with malloc(3))
+				resulting from the successive applications of f.
+
+Param. #1:		The string to map.
+Param. #2: 		The function to apply to each character of s.
+
+Return value: 	The “fresh” string created from the successive applications off.
+*/
+
+char	*ft_strmap(char const *s, char (*f)(char))
+{
+	unsigned int	i;
+	char			*str;
+
+	if (!s || !f)
+		return (NULL);
+	str = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!str)
+		return NULL;
+	i = 0;
+	while (s[i] != '\0')
+	{
+			str[i] = f(s[i]);
+			i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+/*
+//	test function
+char upper(char c) {
+    return c >= 'a' && c <= 'z' ? c - 32 : c;
+}
+
+int	main(void)
+{
+    const char *str = "hello world";
+    char *result = ft_strmap(str, upper);
+
+    if (result != NULL) {
+        printf("Original string: %s\n", str);
+        printf("Mapped string: %s\n", result);
+
+        free(result);
+    } else {
+        printf("ft_strmap failed.\n");
+    }
+    return 0;
+}
+*/
