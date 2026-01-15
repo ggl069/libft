5a12a457e385f18be93f890e0b35e4f62b703ae8
g <gaetanigregorio@gmail.com>
2026-01-15 23:25:08 +0100

    ft_strjoinc

---
+++ ft_strjoin.c
@@ -0,0 +1,52 @@
+// 42 header
+
+#include <stdlib.h>
+#include "libft.h"
+
+/*
+Description     Allocates (with malloc(3)) and returns a “fresh” string end-
+                ing with ’\0’, result of the concatenation of s1 and s2. If
+                the allocation fails the function returns NULL.
+
+Param. #1       The prefix string.
+Param. #2       The suffix string.
+
+Return value    The “fresh” string result of the concatenation of the 2 strings.
+*/
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	if (!s1 || !s2)
+		return (NULL);
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		j++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+/*
+int	main(void)
+{
+	char *str1 = "";
+	char *str2 = "Hello world";
+
+	printf("final string: %s\n", ft_strjoin(str1, str2));
+	return (0);
+}
+*/
